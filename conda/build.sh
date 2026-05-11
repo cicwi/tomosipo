89ca4f6485fcd8b55447e373fe88018f41545428
Alexander Skorikov <alexander.skorikov@gmail.com>
2026-05-11 13:47:40 +0200

    Replace setup.py with pyproject.toml

--- conda/build.sh
+++ conda/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-$PYTHON setup.py install --single-version-externally-managed --record record.txt || exit 1
+set -eux
+
+PIP_DISABLE_PIP_VERSION_CHECK=1 \
+    "${PYTHON}" -m pip install . --no-deps --no-build-isolation --no-index -vv
