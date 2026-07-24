f2ce611e1fd8fd60c3671594479a00e78eade456
Alexander Skorikov <alexander.skorikov@gmail.com>
2026-07-24 14:23:51 +0200

    Update logos
    Simplify while keeping it recognizable. Adapt to potential screen sizes and color themes.

--- doc/conf.py
+++ doc/conf.py
@@ -55,12 +55,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-html_logo = "./img/logo.svg"
+html_logo = "./img/logo_title_dark_theme.svg"
+html_favicon = "./img/logo.svg"
 
 
 # -- Support markdown --------------------------------------------------------
